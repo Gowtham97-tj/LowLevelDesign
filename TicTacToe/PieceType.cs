a498ea8942342fd933c7ced597bd1b6133973993
Gowtham TJ <85794138+Gowtham97-tj@users.noreply.github.com>
2023-04-18 22:59:41 +0530

    Initial Commit for TicTacToe

---
+++ TicTacToe/PieceType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public enum PieceType
+    {
+        x, o
+    }
+}
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToeGame ticTacToeGame = new TicTacToeGame(3);
+            ticTacToeGame.initializeGame();
+            Console.WriteLine("the winner is " + ticTacToeGame.startGame());
+        }
+    }
+}
